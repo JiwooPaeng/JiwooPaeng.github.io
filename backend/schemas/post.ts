41a927f2f4105ed9d7864a54daf1e4caa83e586f
JiwooPaeng <shishalu04@gmail.com>
2023-09-11 11:03:05 +0900

    :sparkles: feat: post/user 스키마 추가

--- backend/schemas/post.ts
+++ backend/schemas/post.ts
@@ -1,65 +1,55 @@
-import {defineField, defineType} from 'sanity'
-
-export default defineType({
-  name: 'post',
+export default {
   title: 'Post',
+  name: 'post',
   type: 'document',
   fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
-    }),
-    defineField({
-      name: 'author',
+    {
       title: 'Author',
+      name: 'author',
       type: 'reference',
-      to: {type: 'author'},
-    }),
-    defineField({
-      name: 'mainImage',
-      title: 'Main image',
+      to: [{type: 'user'}]
+    },
+    {
+      title: 'Photo',
+      name: 'photo',
       type: 'image',
-      options: {
-        hotspot: true,
-      },
-    }),
-    defineField({
-      name: 'categories',
-      title: 'Categories',
+    },
+    {
+      title: 'Likes',
+      name: 'likes',
       type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
-    }),
-    defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    }),
-    defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
-    }),
-  ],
-
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'user'}]
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
     },
-    prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
-    },
-  },
-})
+    {
+      title: 'Comments',
+      name: 'comments',
+      type: 'array',
+      of: [
+        {
+          title: 'Comment',
+          name: 'comment',
+          type: 'document',
+          fields: [
+            {
+              title: 'Author',
+              name: 'author',
+              type: 'reference',
+              to: [{type: 'user'}]
+            },
+            {
+              title: 'Comment',
+              name: 'comment',
+              type: 'string',
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
