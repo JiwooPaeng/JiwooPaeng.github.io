432683e4de747e50c186a1f62c93421a1ffebc91
JiwooPaeng <shishalu04@gmail.com>
2023-09-22 22:32:28 +0900

    :sparkles: feat: header footer Hero 컴포넌트 생성

---
+++ my-blog/src/components/Hero.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import profileImage from '../../public/images/profile.jpg'
+
+export default function Hero() {
+  return (
+    <section className="text-center">
+      <Image 
+        src={profileImage} 
+        alt='Picture of the author' 
+        width={250} 
+        height={250}
+        className="mx-auto rounded-full"
+        priority
+        />
+      <h2 className="text-3xl font-bold mt-2">{"Hi, I'm Jiwoo"}</h2>
+      <h3 className="text-xl font-semibold">Front-end Engineer</h3>
+      <p>사용자에게 더 가까이 다각가고 싶은 개발자</p>
+      <Link href="/contact">
+        <button className="bg-yellow-500 font-bold rounded-xl py-1 px-4 mt-2">contact me</button>
+      </Link>
+    </section>
+  )
+}
