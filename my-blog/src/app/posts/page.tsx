a39cafb727d0bc011943de4eb18adda28009d020
JiwooPaeng <shishalu04@gmail.com>
2023-09-24 14:43:11 +0900

    :zap: feat: posts 페이지에서 filter된 post만 보여줄 수 있도록 로직 작성

--- my-blog/src/app/posts/page.tsx
+++ my-blog/src/app/posts/page.tsx
@@ -1,7 +1,12 @@
-export default function PostsPage() {
+import { getAllPosts } from '@/service/posts'
+import FilterablePosts from '@/components/FilterablePosts'
+
+export default async function PostsPage() {
+  const posts = await getAllPosts();
+  const categories = [...new Set(posts.map(post => post.category))]
+  // category만 담겨있는 배열을 생성
+
   return (
-  <div>
-    posts
-  </div>
+  <FilterablePosts posts={posts} categories={categories} />
   )
 }
